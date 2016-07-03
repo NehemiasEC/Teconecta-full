1a567487d45797ac8e31eb882cad32d7b945b5a9
Nehemias Carranza <neli92santscz@gmail.com>
2016-07-03 11:43:24 -0400

    added meta tags head and first html5 tag structure

--- wp-content/themes/Teconecta/index.php
+++ wp-content/themes/Teconecta/index.php
@@ -3,9 +3,24 @@
     <head>
         <meta charset="UTF-8">
         <title><?php bloginfo("name")?></title>
+        <link rel="stylesheet" href="<?php bloginfo("stylesheet_url")?>">
         <meta name="description" content="<?php bloginfo("description")?>">
+        <link rel="pingback" href="<?php bloginfo("pingback")?>">
     </head>
     <body>
+        <header>
+            <figure>
+                <img src="" alt="Teconecta Logo">
+            </figure>
+            <nav>
+                <!-- navegacion principal-->
+            </nav>
+            <div id="profile">
+                <!-- menu del perfil del indivudo con api de facebook-->
+            </div>
+        </header>
+        <section id="main">
 
+        </section>
     </body>
 </html>

e2d01f4682514bd5b3cb23d5cc28ff73535cf87c
Nehemias Carranza <neli92santscz@gmail.com>
2016-07-03 11:19:58 -0400

    added style configurations, and head tags

--- wp-content/themes/Teconecta/index.php
+++ wp-content/themes/Teconecta/index.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: santcz
- * Date: 7/3/2016
- * Time: 10:48 AM
- */
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title><?php bloginfo("name")?></title>
+        <meta name="description" content="<?php bloginfo("description")?>">
+    </head>
+    <body>
+
+    </body>
+</html>
